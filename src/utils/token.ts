f7893616df425bd5d7830bd66a8dee772aba3799
Daltonic <darlingtongospel@gmail.com>
2024-02-19 21:40:52 +0100

    adding dot env config to the token file

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,7 +1,9 @@
+require('dotenv').config()
 import jwt from 'jsonwebtoken'
 
 export function verifyAccessToken<T>(accessToken: string): T | null {
   try {
+    console.log(process.env.ACCESS_TOKEN_SECRET_KEY)
     const decoded = jwt.verify(
       accessToken,
       process.env.ACCESS_TOKEN_SECRET_KEY!
