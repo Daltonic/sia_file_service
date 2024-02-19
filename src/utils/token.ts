18e48fab8b685ba8f72e6ef973cdbfc56e515aa5
Daltonic <darlingtongospel@gmail.com>
2024-02-19 21:55:45 +0100

    checking access token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -4,10 +4,12 @@ import jwt from 'jsonwebtoken'
 export function verifyAccessToken<T>(accessToken: string): T | null {
   try {
     console.log(process.env.ACCESS_TOKEN_SECRET_KEY)
+    console.log(accessToken)
     const decoded = jwt.verify(
       accessToken,
       process.env.ACCESS_TOKEN_SECRET_KEY!
     ) as T
+    console.log(decoded)
     return decoded
   } catch (e: any) {
     return null
